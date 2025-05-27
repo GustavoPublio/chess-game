3141527485d3741c1e53589eb094495d183ed5dd
Gustavo Públio <gustavopmagalhaes2017@outlook.com>
2025-05-27 18:42:04 -0300

    Criando a classe ChessPosition

---
+++ XadrezConsole/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using ChessConsole.ChessBoard;
+
+namespace Chess
+{
+    class ChessPosition
+    {
+        public char File { get; set; }
+        public int Rank { get; set; }
+
+        public ChessPosition(char file, int rank)
+        {
+            File = file;
+            Rank = rank;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Rank, File - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + File + Rank;
+        }
+    }
+}
--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,19 +9,9 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                ChessBoardClass Board = new ChessBoardClass(8, 8);
-                Board.PlacePiece(new Rook(Board, Color.Black), new Position(0, 0));
-                Board.PlacePiece(new Rook(Board, Color.Black), new Position(1, 3));
-                Board.PlacePiece(new King(Board, Color.Black), new Position(0, 2));
-
-                Screen.PrintChessBoard(Board);
-            }
-            catch (ChessBoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition position = new ChessPosition('c', 7);
+            Console.WriteLine(position);
+            Console.WriteLine(position.ToPosition());
             Console.ReadLine();
         }
     }
